be533db33d2dd2405e24574ef313534b466ee316
GT4933 <nairgovind06@gmail.com>
2026-07-26 03:12:19 +0530

    widget fix

--- src/modules/onboarding/tools/assignTraining.ts
+++ src/modules/onboarding/tools/assignTraining.ts
@@ -1,4 +1,4 @@
-import { ExecutionContext } from '@nitrostack/core';
+import { Tool, Widget, ExecutionContext } from '@nitrostack/core';
 import { readDB, writeDB } from '../../../utils/db.js';
 import { logAudit } from '../../../utils/auditLogger.js';
 import { ExecutionTracker } from '../../../utils/executionTracker.js';
